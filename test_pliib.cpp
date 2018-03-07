74f0cbbde88847a2cdd32b537e8c552844f38f9e
Eric T. Dawson <eric.t.dawson@gmail.com>
2018-03-07 17:17:16 +0000

    Add test for string split

--- test_pliib.cpp
+++ test_pliib.cpp
@@ -24,15 +24,24 @@ TEST_CASE("fast split function performs as expected", "[split]"){
     int* split_sizes;
     split(to_split, ';', ret, retsz, split_sizes);
 
-    for (int i = 0; i < retsz; ++i){
+    //for (int i = 0; i < retsz; ++i){
 
-        cout << '"' << ret[i] << '"' << endl;
-    }
+    //    cout << '"' << ret[i] << '"' << endl;
+    //}
 
 
     REQUIRE(retsz == 5);
     REQUIRE(strcmp(ret[0], "A") == 0);
     REQUIRE(strcmp(ret[1], "StrING") == 0);
-//    REQUIRE(split_sizes[2] == 11);
+    REQUIRE(split_sizes[2] == 11);
     REQUIRE(split_sizes[0] == 1);
+
+    SECTION("split works for strings"){
+        string s = "ANOTHER:Separated:String";
+        vector<string> ret;
+        split(s, ':', ret);
+        REQUIRE(ret.size() == 3);
+        REQUIRE(ret[0].compare("ANOTHER") == 0);
+        REQUIRE(ret[2].compare("String") == 0);
+    }
 }
